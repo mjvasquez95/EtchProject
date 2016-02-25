5cfa2b023e60287b592c0456df73f6612cea020d
mjvasquez95 <mjvm1975@yahoo.com>
2016-02-24 21:32:51 -0700

    Initial commit

---
+++ app/src/main/java/com/layout/settings/Settings.java
@@ -0,0 +1,15 @@
+package com.layout.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.draft.rough.etchproject.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
